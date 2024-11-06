79d9d4948196957349afd597a1e033f9bbe8bdff
aimanalishezan <aimanalishezan22@gmail.com>
2024-11-06 23:51:09 +0600

    worked

--- Man_Ai/app.py
+++ Man_Ai/app.py
@@ -1,6 +1,6 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.llms import ollama
+from langchain_community.llms import Ollama
 
 import streamlit as st
 import os
@@ -8,23 +8,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#langchain tracking
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-#creating cheat bot
-prompt=ChatPromptTemplate.from_messages(
+# Verify environment variable
+langchain_api_key = os.getenv("LANGCHAIN_API_KEY")
+if not langchain_api_key:
+    raise ValueError("LANGCHAIN_API_KEY environment variable not set.")
+os.environ["LANGCHAIN_API_KEY"] = langchain_api_key
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+
+# Creating the chat bot
+prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","you are e helpful assistant.please provide response to the user queries"),
-        ("user","Question:{question}")
+        ("system", "you are a helpful assistant. Please provide responses to user queries"),
+        ("user", "Question: {question}")
     ]
 )
-#streamlit framework
+
+# Streamlit framework
 st.title("Aiman's Man.AI")
-input_text=st.text_input("search the topic you want")
-#llm call
-llm=ollama(model="llama3.2")
-output_parser=StrOutputParser()
-#chain
-chain=prompt|llm|output_parser
+input_text = st.text_input("Search the topic you want")
+
+# LLM call
+llm = Ollama(model="llama3.2")
+output_parser = StrOutputParser()
+
+# Chain
+chain = prompt | llm | output_parser
 if input_text:
-    st.write(chain.invoke({"question":input_text}))
+    st.write(chain.invoke({'question': input_text}))
