b4aeada6f5000516f5ea55aa558197f596b947a8
aimanalishezan <aimanalishezan22@gmail.com>
2024-11-07 00:33:38 +0600

    key

--- Api/api.py
+++ Api/api.py
@@ -1,3 +1,4 @@
+#from langchain.chat_models import ChatOpenAI
 from fastapi import FastAPI
 from langchain.prompts import ChatPromptTemplate
 from langserve import  add_routes
@@ -6,6 +7,18 @@ from dotenv import load_dotenv
 import uvicorn
 import os
 
-load_dotenv()
+load_dotenv()#for loading env variables
+#set open key
+#os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 
-os.environ[""]
+#create the api with fastapi
+api=FastAPI(
+
+    title="langchain Server",
+    version="1.0",
+    description="A simple API server"
+)
+add_routes(
+    api,
+    #ChatOpenAI()
+)
