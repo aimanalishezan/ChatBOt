354faccf42faaffdad45308ed940edf9d39ca8e4
aimanalishezan <aimanalishezan22@gmail.com>
2024-11-08 16:34:14 +0600

    done

--- ChatWithHistory/chat.py
+++ ChatWithHistory/chat.py
@@ -6,7 +6,7 @@ from langchain_core.prompts import ChatPromptTemplate
 
 
 #stramlit structure
-st.title(":robot: MAN_AI" )
+st.title("🤖 MAN.AI" )
 st.write("DEVELOPER: MD AIMAN ALI SHEZAN")
 llm_model=ChatOllama(model="llama3.2")
 system_message=SystemMessagePromptTemplate.from_template("You are a helpful AI Assistant.You are only giving the relivant Answer.On Every correct ans you will have 200$")
@@ -16,7 +16,7 @@ if "chat_history" not in st.session_state:
     st.session_state["chat_history"]=[]
 
 with st.form("input-form"):
-    text=st.text_area("Enter your question")
+    text=st.text_area("Enter Your Question Here")
     submit=st.form_submit_button("Submit")
 
 def gennerate_response(chat_history):
@@ -43,12 +43,12 @@ if submit and text:
         chat_history.append(prompt)
         response=gennerate_response(chat_history)
 
-        st.session_state("chat_history").append({'user':text,'assistant':response})
+        st.session_state['chat_history'].append({'user':text,'assistant':response})
         # st.write('response',response)
         # st.write(st.session_state['chat_history'])
 
 st.write('#Chat History')
 for chat in reversed( st.session_state['chat_history']):
-    st.write(f'**:adult: user**:{chat['user']}')
-    st.write(f'**:human: Assistant**:{chat['assistant']}')
-    st.write('---')
+    st.write(f"**:adult: user**:{chat['user']}")
+    st.write(f"**:brain: Assistant**:{chat['assistant']}")
+    st.write("---")
